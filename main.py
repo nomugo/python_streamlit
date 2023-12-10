afacf36693170066d902c3a96765345b765053a2
NomuGo <kaisernomu@gmail.com>
2023-12-10 15:25:02 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import time
+
+st.title('Streamlit入門')
+
+st.write('プログレスバーの表示')
+'Start!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+     latest_iteration.text(f'Iteration{i+1}')
+     bar.progress(i + 1)
+     time.sleep(0.1)
+
+'Done!'
+
+left_column, right_column = st.columns(2)
+
+button = left_column.button('右カラムに文字を表示')
+
+if button:
+     right_column.write('ボタンが押されました。')
+
+expander1 = st.expander('問い合わせ')
+expander1.write('問い合わせ1')
+expander2 = st.expander('問い合わせ')
+expander2.write('問い合わせ2')
+expander3 = st.expander('問い合わせ')
+expander3.write('問い合わせ3')
